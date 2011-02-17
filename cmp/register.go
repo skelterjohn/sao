e646f044b960beb6786b4462c3af54870df3512f
John Asmuth <jasmuth@gmail.com>
2011-02-17 11:01:39 -0500

    build files, space out the components

---
+++ cmp/register.go
@@ -0,0 +1,10 @@
+package cmp
+
+import (
+	"github.com/hoisie/web.go"
+)
+
+func RegisterAll() {
+    web.Get("/Button?(.*)", Button)
+	web.Get("/TextEdit?(.*)", TextEdit)
+}
---
+++ cmp/text.go
@@ -0,0 +1,5 @@
+package cmp
+
+func TextEdit(query string) (code string) {
+	return `<textarea style="width:375px; height:375px;"></textarea>`
+}
